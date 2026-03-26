f0be6415d8051ebd5724b9c40e5f2b89b77ba466
Mohit Kumar <theprincemohit@gmail.com>
2026-03-27 01:23:00 +0530

    route(route): define routing

---
+++ hooks/useAuthRoute.ts
@@ -0,0 +1,45 @@
+import { useRouter } from 'expo-router';
+import { useEffect } from 'react';
+
+import { getToken } from '@/api/keychain';
+
+export function useProtectedRoute() {
+  const router = useRouter();
+
+  useEffect(() => {
+    let isMounted = true;
+    const checkAuth = async () => {
+      const token = await getToken('userToken');
+      console.log('Token in useProtectedRoute:', token);
+      if (!token && isMounted) {
+        router.replace('/login');
+      }
+    };
+
+    checkAuth();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [router]);
+}
+
+export function usePublicRoute() {
+  const router = useRouter();
+
+  useEffect(() => {
+    let isMounted = true;
+    const checkAuth = async () => {
+      const token = await getToken('userToken');
+      if (token && isMounted) {
+        router.replace('/(tabs)/business');
+      }
+    };
+
+    checkAuth();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [router]);
+}
